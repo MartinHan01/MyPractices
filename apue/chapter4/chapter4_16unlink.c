c8614952800078d10c9c1df5c51efc23be7f28cd
MartinHan <hanhan145@163.com>
2017-08-02 16:35:53 +0800

    update 4_16

--- apue/chapter4/chapter4_16unlink.c
+++ apue/chapter4/chapter4_16unlink.c
@@ -1,11 +1,18 @@
 #include "my_apue.h"
 
+char buf[] = "1234567890";
+
 int main(int argc, char const *argv[]) {
 	int fd;
 	fd = open("tempfile", O_RDWR | O_CREAT, 0755);
 	if(fd == -1) {
 		perror("open");
 		exit(1);
+	}
+	if(write(fd, buf,strlen(buf)) == -1) {
+		printf("write failed\n");
+	} else {
+		printf("write success\n");
 	}
 	printf("create tempfile\n");
 	sleep(10);
@@ -16,6 +23,12 @@ int main(int argc, char const *argv[]) {
 	printf("has unlink\n");
 	printf("start sleep\n");
 	sleep(15);
+	if(write(fd, buf,strlen(buf)) == -1) {
+		printf("second write failed\n");
+		perror("second write");
+	} else {
+		printf("second write success\n");
+	}
 	printf("done\n");
 
 	return 0;
