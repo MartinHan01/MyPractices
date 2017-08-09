dcf66c19c3f67c09a7a5b9db2c6853bd6dd67444
MartinHan <hanhan145@163.com>
2017-08-10 00:06:22 +0800

    update dev

--- apue/chapter4/chapter4_24getcwd.c
+++ apue/chapter4/chapter4_24getcwd.c
@@ -2,13 +2,13 @@
 
 int main(int argc, char *argv[]) {
 
-    char *ptr;
+    char ptr[256];
     size_t size;
     if(chdir("/tmp") < 0) {
         perror("chdir failed");
         exit(1);
     }
-    ptr = path_alloc(&size);
+    // ptr = path_alloc(&size);
     if(getcwd(ptr, size) == NULL) {
         perror("getcwd");
     }
---
+++ apue/chapter4/chapter4_25dev.c
@@ -0,0 +1,23 @@
+#include "my_apue.h"
+
+int main(int argc , char *argv[]) {
+
+    int i;
+    struct stat buf;
+    for(i = 1; i < argc; i++) {
+        printf("%s : ", argv[i]);
+        if(stat(argv[i], &buf) < 0) {
+            perror("stat error");
+            continue;
+        }
+        printf("dev = %d/%d", major(buf.st_dev),minor(buf.st_rdev));
+        if(S_ISCHR(buf.st_mode) || S_ISBLK(buf.st_mode)) {
+            printf(" (%s) rdev = %d/%d", 
+            (S_ISCHR(buf.st_mode)) ? "character" : "block", major(buf.st_dev), minor(buf.st_rdev));
+
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
