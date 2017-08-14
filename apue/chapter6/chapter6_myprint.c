cda3f12b1baceaaa6fa34a31470a7d30ae85be65
MartinHan <hanhan145@163.com>
2017-08-14 23:53:20 +0800

    add my practice on chapter 6

---
+++ apue/chapter6/chapter6_myprint.c
@@ -0,0 +1,13 @@
+#include "my_apue.h"
+
+int main(int argc, char *argv[]) {
+
+    struct passwd *ptr;
+    while((ptr = getpwent()) != NULL) {
+        printf("%s  %s  %d  %d  %s  %s\n", 
+            ptr->pw_name, ptr->pw_passwd, ptr->pw_uid, ptr->pw_gid, ptr->pw_dir, ptr->pw_shell);
+    }
+    endpwent();
+    
+    return 0;
+}
