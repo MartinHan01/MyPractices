95a84a5245fc9be21a3bb4f267166d12770e7705
MartinHan <hanhan145@163.com>
2017-08-14 23:54:04 +0800

    update code

--- apue/chapter6/chapter6_myprint.c
+++ apue/chapter6/chapter6_myprint.c
@@ -3,6 +3,7 @@
 int main(int argc, char *argv[]) {
 
     struct passwd *ptr;
+    setpwent();
     while((ptr = getpwent()) != NULL) {
         printf("%s  %s  %d  %d  %s  %s\n", 
             ptr->pw_name, ptr->pw_passwd, ptr->pw_uid, ptr->pw_gid, ptr->pw_dir, ptr->pw_shell);
