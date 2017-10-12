c9df563c1f0ad0be03b02b33a2a50f15bef1fed1
MartinHan <hanhan145@163.com>
2017-10-12 16:27:44 +0800

    update a square

--- os/tolset/harib_my/bootpack.c
+++ os/tolset/harib_my/bootpack.c
@@ -8,15 +8,34 @@ void io_out8(int port,int data);
 
 void init_palette(void);
 void set_palette(int, int, char rgb[]);
-void hlt(void);
+void hlt_loop(void);
+void boxfill(char* vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+
+#define COL8_000000		0     /*  0:黒 */
+#define COL8_FF0000		1     /*  1:明るい赤 */
+#define COL8_00FF00		2     /*  2:明るい緑 */
+#define COL8_FFFF00		3     /*  3:明るい黄色 */
+#define COL8_0000FF		4     /*  4:明るい青 */
+#define COL8_FF00FF		5     /*  5:明るい紫 */
+#define COL8_00FFFF		6     /*  6:明るい水色 */
+#define COL8_FFFFFF		7     /*  7:白 */
+#define COL8_C6C6C6		8     /*  8:明るい灰色 */
+#define COL8_840000		9     /*  9:暗い赤 */
+#define COL8_008400		10    /* 10:暗い緑 */
+#define COL8_848400		11    /* 11:暗い黄色 */
+#define COL8_000084		12    /* 12:暗い青 */
+#define COL8_840084		13    /* 13:暗い紫 */
+#define COL8_008484		14    /* 14:暗い水色 */
+#define COL8_848484		15    /* 15:暗い灰色 */
 
 void HariMain(void) {
     int i = 0;
     char *p = (char*)0xa0000;
     init_palette();
-    for(i = 0 ; i <= (0xfA00 - 0x140); i++) {
-        p[i] = i & 0x0f;
-    }
+
+    boxfill(p, 320, COL8_FF0000, 20, 20, 120, 120);
+
+    
     hlt_loop();
 }
 
@@ -59,6 +78,15 @@ void set_palette(int start, int end,char rgb[]) {
 
     io_store_eflags(eflags);
     return ;
+}
+
+void boxfill(char* vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1) {
+    int x,y;
+    for(x = x0; x <= x1; x++) {
+        for(y = y0; y < y1; y++) {
+            vram[x + y * xsize] = c;
+        }
+    }
 }
 
 void hlt_loop(void) {
