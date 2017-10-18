9a35b80be956a60186aac3db5fa59c3eea662374
MartinHan <hanhan145@163.com>
2017-10-18 11:26:25 +0800

    finished day6-9

--- os/tolset/harib_my/bootpack.h
+++ os/tolset/harib_my/bootpack.h
@@ -1,75 +1,68 @@
 /* asmhead.nas */
-extern char hankaku[4096];
-struct BOOTINFO {
-    char cyls, leds, vmode;
-    short scrnx, scrny;
-    char *vram;
+struct BOOTINFO { /* 0x0ff0-0x0fff */
+	char cyls; /* ƒu[ƒgƒZƒNƒ^‚Í‚Ç‚±‚Ü‚ÅƒfƒBƒXƒN‚ğ“Ç‚ñ‚¾‚Ì‚© */
+	char leds; /* ƒu[ƒg‚ÌƒL[ƒ{[ƒh‚ÌLED‚Ìó‘Ô */
+	char vmode; /* ƒrƒfƒIƒ‚[ƒh  ‰½ƒrƒbƒgƒJƒ‰[‚© */
+	char reserve;
+	short scrnx, scrny; /* ‰æ–Ê‰ğ‘œ“x */
+	char *vram;
 };
 #define ADR_BOOTINFO	0x00000ff0
 
 /* naskfunc.nas */
 void io_hlt(void);
-void write_mem8(int, int);
-int io_load_eflags(void);
 void io_cli(void);
-void io_store_eflags(int eflags);
+void io_sti(void);
 void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
 void load_gdtr(int limit, int addr);
 void load_idtr(int limit, int addr);
 void asm_inthandler21(void);
 void asm_inthandler27(void);
-void io_sti(void);
-
-/* bootpack.c */
-void hlt_loop(void);
+void asm_inthandler2c(void);
 
 /* graphic.c */
 void init_palette(void);
-void set_palette(int, int, unsigned char *rgb);
-void boxfill8(char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-void putfont8_asc(char *vram, int xsize, int x, int y, unsigned char c, unsigned char *s);
-void putfont8(char *vram, int xsize, int x, int y, unsigned char c,char *font);
-void init_screen(char *vram, int x, int y);
+void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void init_screen8(char *vram, int x, int y);
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
 void init_mouse_cursor8(char *mouse, char bc);
 void putblock8_8(char *vram, int vxsize, int pxsize,
-    int pysize, int px0, int py0, char *buf, int bxsize);
-void printtest(void);
-
-
-#define COL8_000000 0  /*  0:é»’ */
-#define COL8_FF0000 1  /*  1:æ˜ã‚‹ã„èµ¤ */
-#define COL8_00FF00 2  /*  2:æ˜ã‚‹ã„ç·‘ */
-#define COL8_FFFF00 3  /*  3:æ˜ã‚‹ã„é»„è‰² */
-#define COL8_0000FF 4  /*  4:æ˜ã‚‹ã„é’ */
-#define COL8_FF00FF 5  /*  5:æ˜ã‚‹ã„ç´« */
-#define COL8_00FFFF 6  /*  6:æ˜ã‚‹ã„æ°´è‰² */
-#define COL8_FFFFFF 7  /*  7:ç™½ */
-#define COL8_C6C6C6 8  /*  8:æ˜ã‚‹ã„ç°è‰² */
-#define COL8_840000 9  /*  9:æš—ã„èµ¤ */
-#define COL8_008400 10 /* 10:æš—ã„ç·‘ */
-#define COL8_848400 11 /* 11:æš—ã„é»„è‰² */
-#define COL8_000084 12 /* 12:æš—ã„é’ */
-#define COL8_840084 13 /* 13:æš—ã„ç´« */
-#define COL8_008484 14 /* 14:æš—ã„æ°´è‰² */
-#define COL8_848484 15 /* 15:æš—ã„ç°è‰² */
+	int pysize, int px0, int py0, char *buf, int bxsize);
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
 
 /* dsctbl.c */
 struct SEGMENT_DESCRIPTOR {
-    short limit_low, base_low;
-    char base_mid, access_right;
-    char limit_high, base_high;
+	short limit_low, base_low;
+	char base_mid, access_right;
+	char limit_high, base_high;
 };
-
 struct GATE_DESCRIPTOR {
-    short offset_low,selector;
-    char dw_count, access_right;
-    short offset_high;
+	short offset_low, selector;
+	char dw_count, access_right;
+	short offset_high;
 };
-
 void init_gdtidt(void);
 void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
 void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
-
 #define ADR_IDT			0x0026f800
 #define LIMIT_IDT		0x000007ff
 #define ADR_GDT			0x00270000
@@ -78,12 +71,13 @@ void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 #define LIMIT_BOTPAK	0x0007ffff
 #define AR_DATA32_RW	0x4092
 #define AR_CODE32_ER	0x409a
-
+#define AR_INTGATE32	0x008e
 
 /* int.c */
 void init_pic(void);
 void inthandler21(int *esp);
 void inthandler27(int *esp);
+void inthandler2c(int *esp);
 #define PIC0_ICW1		0x0020
 #define PIC0_OCW2		0x0020
 #define PIC0_IMR		0x0021
@@ -96,11 +90,3 @@ void inthandler27(int *esp);
 #define PIC1_ICW2		0x00a1
 #define PIC1_ICW3		0x00a1
 #define PIC1_ICW4		0x00a1
-#define AR_INTGATE32	0x008e
-
-
-
-
-
-
-
--- os/tolset/harib_my/dsctbl.c
+++ os/tolset/harib_my/dsctbl.c
@@ -1,27 +1,38 @@
+/* GDTÇ‚IDTÇ»Ç«ÇÃÅA descriptor table ä÷åW */
+
 #include "bootpack.h"
 
-void init_gdtidt(void) {
-    struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *)ADR_GDT;
-    struct GATE_DESCRIPTOR *idt    = (struct GATE_DESCRIPTOR    *)ADR_IDT;
-    int i;
-    for(i = 0; i < 8192; i++) {
-        set_segmdesc(gdt + i, 0, 0, 0);
-    }
-    set_segmdesc(gdt + 1,0xffffffff, 0x00000000, AR_DATA32_RW);
-    set_segmdesc(gdt + 2,LIMIT_BOTPAK, ADR_BOTPAK, AR_CODE32_ER);
-    load_gdtr(LIMIT_GDT, ADR_GDT);
-    for(i = 0; i < 256; i++) {
-        set_gatedesc(idt + i, 0, 0, 0);
-    }
-    load_idtr(LIMIT_IDT, ADR_IDT);
-    
-    set_gatedesc(idt + 0x21, (int)asm_inthandler21, 2 * 8, AR_INTGATE32);
-    set_gatedesc(idt + 0x27, (int)asm_inthandler27, 2 * 8, AR_INTGATE32);
-    return ;
+void init_gdtidt(void)
+{
+	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
+	struct GATE_DESCRIPTOR    *idt = (struct GATE_DESCRIPTOR    *) ADR_IDT;
+	int i;
+
+	/* GDTÇÃèâä˙âª */
+	for (i = 0; i <= LIMIT_GDT / 8; i++) {
+		set_segmdesc(gdt + i, 0, 0, 0);
+	}
+	set_segmdesc(gdt + 1, 0xffffffff,   0x00000000, AR_DATA32_RW);
+	set_segmdesc(gdt + 2, LIMIT_BOTPAK, ADR_BOTPAK, AR_CODE32_ER);
+	load_gdtr(LIMIT_GDT, ADR_GDT);
+
+	/* IDTÇÃèâä˙âª */
+	for (i = 0; i <= LIMIT_IDT / 8; i++) {
+		set_gatedesc(idt + i, 0, 0, 0);
+	}
+	load_idtr(LIMIT_IDT, ADR_IDT);
+
+	/* IDTÇÃê›íË */
+	set_gatedesc(idt + 0x21, (int) asm_inthandler21, 2 * 8, AR_INTGATE32);
+	set_gatedesc(idt + 0x27, (int) asm_inthandler27, 2 * 8, AR_INTGATE32);
+	set_gatedesc(idt + 0x2c, (int) asm_inthandler2c, 2 * 8, AR_INTGATE32);
+
+	return;
 }
 
-void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar) {
-    if (limit > 0xfffff) {
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar)
+{
+	if (limit > 0xfffff) {
 		ar |= 0x8000; /* G_bit = 1 */
 		limit /= 0x1000;
 	}
@@ -34,13 +45,12 @@ void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, i
 	return;
 }
 
-void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar) {
-    gd->offset_low    = offset & 0xff;
-    gd->selector      = selector;
-    gd->dw_count      = (ar >> 8) & 0xff;
-    gd->access_right  = ar & 0xff;
-    gd->offset_high   = (offset >> 16) & 0xffff;
-    return ;
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar)
+{
+	gd->offset_low   = offset & 0xffff;
+	gd->selector     = selector;
+	gd->dw_count     = (ar >> 8) & 0xff;
+	gd->access_right = ar & 0xff;
+	gd->offset_high  = (offset >> 16) & 0xffff;
+	return;
 }
-
-
