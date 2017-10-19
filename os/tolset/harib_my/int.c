2632751eb0c88570f7025940d0193ee2c7f6d192
MartinHan <hanhan145@163.com>
2017-10-19 15:10:26 +0800

    finished day7-7

--- os/tolset/harib_my/int.c
+++ os/tolset/harib_my/int.c
@@ -37,11 +37,15 @@ void inthandler21(int *esp)
     return ;
 }
 
+struct FIFO8 mousefifo;
+
 void inthandler2c(int *esp)
 {
-	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-	boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 48, 32 * 8 - 1, 15);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 48, COL8_FFFFFF, "INT 2C (IRQ-12) : PS/2 mouse");
+    unsigned char data;
+    io_out8(PIC1_OCW2, 0x64);
+    io_out8(PIC0_OCW2, 0x62);
+    data = io_in8(PORT_KEYDAT);
+    fifo8_put(&mousefifo,data);
 	return;
 }
 
