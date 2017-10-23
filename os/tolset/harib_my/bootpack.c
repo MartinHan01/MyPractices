71a9c9c0eaf7339b6e0d95c264ca1e1442e3f403
MartinHan <hanhan145@163.com>
2017-10-23 16:00:04 +0800

    finish day9-1

--- os/tolset/harib_my/bootpack.c
+++ os/tolset/harib_my/bootpack.c
@@ -54,9 +54,40 @@ void HariMain(void)
                 io_sti();
                 if (mouse_decode(&mdec, i) != 0) {
 					/* データが3バイト揃ったので表示 */
-					sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
-					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
-					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+					sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+					if ((mdec.btn & 0x01) != 0) {
+						s[1] = 'L';
+					}
+					if ((mdec.btn & 0x02) != 0) {
+						s[3] = 'R';
+					}
+					if ((mdec.btn & 0x04) != 0) {
+						s[2] = 'C';
+					}
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
+                    putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+                    
+                    /* マウスカーソルの移動 */
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx + 15, my + 15); /* マウス消す */
+					mx += mdec.x;
+					my += mdec.y;
+					if (mx < 0) {
+						mx = 0;
+					}
+					if (my < 0) {
+						my = 0;
+					}
+					if (mx > binfo->scrnx - 16) {
+						mx = binfo->scrnx - 16;
+					}
+					if (my > binfo->scrny - 16) {
+						my = binfo->scrny - 16;
+					}
+					sprintf(s, "(%3d, %3d)", mx, my);
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 79, 15); /* 座標消す */
+					putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s); /* 座標書く */
+					putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16); /* マウス描く */
+
 				}
 
 				
@@ -68,77 +99,8 @@ void HariMain(void)
 
 
 
-#define PORT_KEYDAT				0x0060
-#define PORT_KEYSTA				0x0064
-#define PORT_KEYCMD				0x0064
-#define KEYSTA_SEND_NOTREADY	0x02
-#define KEYCMD_WRITE_MODE		0x60
-#define KBC_MODE				0x47
 
 
-void wait_KBC_sendready(void)
-{
-	/* キーボードコントローラがデータ送信可能になるのを待つ */
-	for (;;) {
-		if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0) {
-			break;
-		}
-	}
-	return;
-}
-
-#define KEYCMD_SENDTO_MOUSE		0xd4
-#define MOUSECMD_ENABLE			0xf4
-
-void enable_mouse(struct MOUSE_DEC *mdec)
-{
-	/* マウス有効 */
-	wait_KBC_sendready();
-	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
-	wait_KBC_sendready();
-	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
-	/* うまくいくとACK(0xfa)が送信されてくる */
-	mdec->phase = 0; /* マウスの0xfaを待っている段階 */
-	return;
-}
-
-void init_keyboard(void)
-{
-	/* �L�[�{�[�h�R���g���[���̏����� */
-	wait_KBC_sendready();
-	io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
-	wait_KBC_sendready();
-	io_out8(PORT_KEYDAT, KBC_MODE);
-	return;
-}
 
 
-int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
-{
-	if (mdec->phase == 0) {
-		/* マウスの0xfaを待っている段階 */
-		if (dat == 0xfa) {
-			mdec->phase = 1;
-		}
-		return 0;
-	}
-	if (mdec->phase == 1) {
-		/* マウスの1バイト目を待っている段階 */
-		mdec->buf[0] = dat;
-		mdec->phase = 2;
-		return 0;
-	}
-	if (mdec->phase == 2) {
-		/* マウスの2バイト目を待っている段階 */
-		mdec->buf[1] = dat;
-		mdec->phase = 3;
-		return 0;
-	}
-	if (mdec->phase == 3) {
-		/* マウスの3バイト目を待っている段階 */
-		mdec->buf[2] = dat;
-		mdec->phase = 1;
-		return 1;
-	}
-	return -1; /* ここに来ることはないはず */
-}
+
--- os/tolset/harib_my/bootpack.h
+++ os/tolset/harib_my/bootpack.h
@@ -1,3 +1,16 @@
+#define PORT_KEYDAT				0x0060
+#define PORT_KEYSTA				0x0064
+#define PORT_KEYCMD				0x0064
+#define KEYSTA_SEND_NOTREADY	0x02
+#define KEYCMD_WRITE_MODE		0x60
+#define KBC_MODE				0x47
+
+
+
+#define KEYCMD_SENDTO_MOUSE		0xd4
+#define MOUSECMD_ENABLE			0xf4
+
+
 /* asmhead.nas */
 struct BOOTINFO {  
 	char cyls;     
@@ -76,9 +89,7 @@ void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 
 /* int.c */
 void init_pic(void);
-void inthandler21(int *esp);
-void inthandler27(int *esp);
-void inthandler2c(int *esp);
+
 #define PIC0_ICW1		0x0020
 #define PIC0_OCW2		0x0020
 #define PIC0_IMR		0x0021
@@ -109,7 +120,8 @@ int fifo8_status(struct FIFO8 *fifo);
 /* bootpack.h */
 
 struct MOUSE_DEC {
-	unsigned char buf[3], phase;
+    unsigned char buf[3], phase;
+    int x, y, btn;
 };
 
 void enable_mouse(struct MOUSE_DEC *mdec);
@@ -117,3 +129,7 @@ void wait_KBC_sendready(void);
 void init_keyboard(void);
 int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 
+/* mouse */
+void inthandler21(int *esp);
+void inthandler27(int *esp);
+void inthandler2c(int *esp);
