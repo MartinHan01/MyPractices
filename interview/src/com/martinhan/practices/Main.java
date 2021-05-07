5a24fcd4da0ea29ffa72b515a6242ca5ee96abab
You <hanhan145@163.com>
2021-05-08 04:22:32 +0800

    update

---
+++ interview/src/com/martinhan/practices/Main.java
@@ -0,0 +1,8 @@
+package com.martinhan.practices;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ interview/src/com/martinhan/practices/ShortTest.java
@@ -0,0 +1,11 @@
+package com.martinhan.practices;
+
+public class ShortTest {
+    public static void main(String[] args) {
+        short s1 = 1;
+//        s1 = s1 + 1;
+        s1 = (short) (s1 + 1);
+        short s2 = 1;
+        s2 += 1;
+    }
+}
